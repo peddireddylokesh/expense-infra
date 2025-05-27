a277b208e068b3580a220428429a35cf4599c654
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-05-27 13:13:19 +0530

    jenkins-practice

--- 70-ingress-alb/main.tf
+++ 70-ingress-alb/main.tf
@@ -1,24 +1,26 @@
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
   internal = false
-  # expense-dev-app-alb
+
   name    = "${var.project_name}-${var.environment}-ingress-alb"
   vpc_id  = data.aws_ssm_parameter.vpc_id.value
   subnets = local.public_subnet_ids
+
   create_security_group = false
-  security_groups = [local.alb_ingress_sg_id]
+  security_groups       = [local.alb_ingress_sg_id]
   enable_deletion_protection = false
+
   tags = merge(
     var.common_tags,
     {
-        Name = "${var.project_name}-${var.environment}-ingress-alb"
+      Name = "${var.project_name}-${var.environment}-ingress-alb"
     }
   )
 }
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn = module.alb.arn
-  port              = "443"
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = local.ingress_alb_certificate_arn
@@ -39,7 +41,6 @@ resource "aws_route53_record" "web_alb" {
   name    = "expense-${var.environment}.${var.domain_name}"
   type    = "A"
 
-  # these are ALB DNS name and zone information
   alias {
     name                   = module.alb.dns_name
     zone_id                = module.alb.zone_id
@@ -47,6 +48,25 @@ resource "aws_route53_record" "web_alb" {
   }
 }
 
+resource "aws_lb_target_group" "frontend" {
+  name        = local.resource_name
+  port        = 8080
+  protocol    = "HTTP"
+  vpc_id      = local.vpc_id
+  target_type = "ip"
+  deregistration_delay = 60
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    protocol            = "HTTP"
+    port                = 8080
+    path                = "/"
+    matcher             = "200-299"
+    interval            = 10
+  }
+}
 
 resource "aws_lb_listener_rule" "frontend" {
   listener_arn = aws_lb_listener.https.arn
@@ -61,27 +81,5 @@ resource "aws_lb_listener_rule" "frontend" {
     host_header {
       values = ["expense-${var.environment}.${var.domain_name}"]
     }
-  }
-}
-
-
-
-resource "aws_lb_target_group" "frontend" {
-  name     = local.resource_name
-  port     = 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60
-  target_type = "ip"
-
-  health_check {
-    healthy_threshold = 2
-    unhealthy_threshold = 2
-    timeout = 5
-    protocol = "HTTP"
-    port = 8080
-    path = "/"
-    matcher = "200-299"
-    interval = 10
   }
 }
