4521e77f53398d90d2dfcd8835a53671749bac9d
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-06-13 11:31:25 +0530

    jenkins-practice

--- 60-acm/main.tf
+++ 60-acm/main.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "expense" {
-  domain_name       = "*.${var.domain_name}"
+  domain_name       = "*.${var.domain_name}" # *.lokeshportfo.site
   validation_method = "DNS"
 
   tags = merge(
