703d2ae5683dd90690c229e690d09e3e26549f68
peddireddylokesh <peddireddylokesh999@gmail.com>
2025-06-17 12:07:19 +0530

    jenkins-practice

--- 60-ingress-alb/paramters.tf
+++ 60-ingress-alb/paramters.tf
@@ -1,6 +1,6 @@
 resource "aws_ssm_parameter" "https_certificate_arn" {
   name      = "/${var.project_name}/${var.environment}/https_certificate_arn"
   type      = "String"
-  value     = data.aws_acm_certificate.https_cert.arn
+  value     = data.aws_acm_certificate.https_certificate_arn.arn
   overwrite = true
 }
